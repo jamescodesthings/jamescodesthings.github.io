c72b357b9fc2b51146bd11e3dd5ec18d7757e292
James Macmillan <jamescodesthings@gmail.com>
2023-02-22 10:12:01 +0000

    CLEANUP spacing

--- src/components/headings/PageHeading.tsx
+++ src/components/headings/PageHeading.tsx
@@ -9,7 +9,7 @@ type PageHeadingProps = {
   icon: ReactNode;
 };
 export const PageHeading = ({ children, icon }: PropsWithChildren<PageHeadingProps>) => (
-  <div className={`${typography.headingWithIcon} ${typography.lg} ${spacing.mtDouble}`}>
+  <div className={`${typography.headingWithIcon} ${typography.lg} ${spacing.mtDouble} ${spacing.mbAlmostDouble}`}>
     {icon}
     <h1 className={`${spacing.mlHalf} ${typography.default}`}>{children}</h1>
   </div>
