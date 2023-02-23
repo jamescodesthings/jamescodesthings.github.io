1efae9dca5f35f8e7a6b3947c53b732315ef28be
James Macmillan <jamescodesthings@gmail.com>
2023-02-23 12:05:41 +0000

    ADD error page

--- src/routes/ErrorPage.tsx
+++ src/routes/ErrorPage.tsx
@@ -1,25 +1,45 @@
-import { useRouteError } from 'react-router-dom';
+import { Link, useRouteError } from 'react-router-dom';
+
+import styles from './ErrorPage.module.pcss';
 
 /**
  * Error Page
- * @todo improve
  * @constructor
  */
 export default function ErrorPage() {
   const error: any = useRouteError();
 
+  let title = 'Oops!';
+  let leader;
+  let message = <>Sorry, something went wrong.</>;
+
   if (error?.status === 404) {
     console.log('Route not found');
+    const missing = window.location.pathname;
+    title = 'Where are you going?';
+    leader = 'This is embarrassing';
+    message = (
+      <>
+        The page at <span className={styles.highlight}>{missing}</span> isn&apos;t there anymore.
+      </>
+    );
   }
   console.error(error);
 
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error?.statusText || error?.message}</i>
-      </p>
-    </div>
+    <main className={`${styles.main}`}>
+      <article className={`${styles.errorWrapper}`}>
+        <h1 className={`${styles.title}`}>{title}</h1>
+        {leader && <p className={`${styles.leader}`}>{leader}</p>}
+        <p className={`${styles.message}`}>{message}</p>
+        <p className={`${styles.linkBackMessage}`}>
+          My guess is you want to go{' '}
+          <Link to={'/'} className={`${styles.link}`}>
+            Home
+          </Link>
+          .
+        </p>
+      </article>
+    </main>
   );
 }
