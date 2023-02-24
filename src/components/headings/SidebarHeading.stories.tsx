65504d19a7e0ad63e59321260621a7eea8c0153b
James Macmillan <jamescodesthings@gmail.com>
2023-02-24 14:43:01 +0000

    FIX missed one icon

--- src/components/headings/SidebarHeading.stories.tsx
+++ src/components/headings/SidebarHeading.stories.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import spacing from '../../styles/spacing.module.pcss';
 
-import { CoverLetterIcon } from '../icons/CoverLettericon';
+import Icon from '../../assets/svg/jamicons/newsletter.svg';
 import { SidebarHeading } from './SidebarHeading';
 
 export default {
@@ -15,13 +15,13 @@ const Template: ComponentStory<typeof SidebarHeading> = args => <SidebarHeading 
 export const Default = Template.bind({});
 Default.decorators = [story => <div className={`${spacing.mall}`}>{story()}</div>];
 Default.args = {
-  icon: <CoverLetterIcon />,
+  icon: <Icon />,
   title: 'Cover Letter',
 };
 export const WithSubheading = Template.bind({});
 WithSubheading.decorators = [...Default.decorators];
 WithSubheading.args = {
-  icon: <CoverLetterIcon />,
+  icon: <Icon />,
   title: 'Cover Letter',
   children: <>Subhead</>,
 };
