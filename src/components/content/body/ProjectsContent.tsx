ace867fb95697794a48b8d2c7bc5d8bb33d41dc1
James Macmillan <jamescodesthings@gmail.com>
2023-03-02 14:27:42 +0000

    scaffold projects data

--- src/components/content/body/ProjectsContent.tsx
+++ src/components/content/body/ProjectsContent.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
+import dayjs from 'dayjs';
 import { Prose } from '../../typography/Prose';
+
+const projects = [
+  { employer: 'Engineius', name: 'Portal Architecture', from: dayjs('2021-01-01'), summary: [''] },
+  { employer: 'Engineius', name: 'Mobile App', from: dayjs('2020-01-01'), summary: [''] },
+  { employer: 'Packt', name: 'Subscription App', from: dayjs('2018-01-01'), summary: [''] },
+  { employer: 'Severn Trent', name: 'STUCA', from: dayjs('2016-01-01'), summary: [''] },
+  { employer: 'Severn Trent', name: 'DWSP', from: dayjs('2014-01-01'), summary: [''] },
+  { employer: 'Severn Trent', name: 'Portal Rebrand', from: dayjs('2012-01-01'), summary: [''] },
+];
 
 export const ProjectsContent = () => {
   return (
